a5a3b987efe4d8f473d271aa7d84bb50f9ab9cc7
g.ohenry.mendez <g.ohenry.mendez@accenture.com>
2018-09-20 17:34:34 -0500

    Se grega ejercicio js

---
+++ public_html/LearnJS.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//document.write("<span class='mexico'>Hello World, im an imported js file</span>");
+//alert("This is an alert box");
+
+var x="uno";
+let y="dos";
+const pi=3.141516;
+document.write(x+"<br/>");
+x=1;
+var armas2manos =["Two Handed Sword","Battle Axe","War Mace"]; //esto es un array
+var player = {arma:"Pointy Stick", armadura:"Leather Armor", hp:100, mana:100};// esto es un objeto
+document.write(x+pi+"<br/>");
+document.write("You chose: "+armas2manos[1]+"<br/>");
+document.write("You are currently armed with: "+player.arma);
+//this is a coment :)
+/* these are
+ * also comments
+ * :)
+ */
