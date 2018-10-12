1876520427b1f1668893016baa0ce067577a2a51
g.ohenry.mendez <g.ohenry.mendez@accenture.com>
2018-10-12 13:47:15 -0500

    Se grega ejercicio js

--- public_html/js/lirogame.js
+++ public_html/js/lirogame.js
@@ -19,7 +19,7 @@ var _cuero = new armadura("Cuero",3,3);
 var _placas = new armadura("Placas",4,6);
 //jugadores
 var user=new player(100,100,_mandoble,_placas,"Hagane");
-var orc=new player(10,0,_espada,_cuero,"Orcules");
+var orc=new player(25,0,_espada,_cuero,"Orcules");
 
 
 // Ejecucion del Bisne
@@ -36,6 +36,84 @@ var str1=str0.substr(1,6); //23.456 (index start --> index_start + lenght)
 var str2=str0.substring(0,str0.indexOf(".")+3); //123.45 (index start --> index end)
 document.write(str1+"<br/>");
 document.write(str2+"<br/>");
+
+//var playa = setInterval(showPlayerHP,3000);
+//var playa2= setInterval(deductPlayerHP,1500);
+//var deductphp=setInterval(deductPlayerHP,1001);
+var playerHP_anim=setInterval(playerHP,1000);
+var inienemyhp=orc.hp;
+var enemyHP_anim=setInterval(enemyHP,1000);
+var deductorchp=setInterval(deductOrcHP,1001);
+var counter=0;
+var a=0;
+var tbl2 = document.getElementById("tabla2");
+var tbl2_div_arr=tbl2.getElementsByTagName("div");
+
+var ciclictt=setInterval(targettags,1000);
+
+function targettags(){
+    tbl2_div_arr[a].innerHTML=counter;
+    if(counter%2===0){
+        tbl2_div_arr[a].style.color="blue";
+        
+    }
+    else{
+        tbl2_div_arr[a].style.color="red";
+    }
+        counter++;
+        a++;
+    if (a===tbl2_div_arr.length){
+        a=0;
+    }
+}
+
+
+function printTime() {
+    var d = new Date();
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var secs = d.getSeconds();
+    
+    var timer=document.getElementById("tiempo");
+    timer.innerHTML=hours+":"+mins+":"+secs;
+    
+    //document.body.innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
+function printTime12(){
+    var d = new Date();
+    if (d.getHours()>12){
+        var hora=((d.getHours()-12)<10)?"0"+(d.getHours()-12):(d.getHours()-12);
+    }
+    else {
+        var hora=(d.getHours()<10)? "0"+d.getHours():d.getHours() ;
+    }
+    var min=(d.getMinutes()<10)? "0"+d.getMinutes():d.getMinutes() ;
+    var seg=(d.getSeconds()<10)? "0"+d.getSeconds():d.getSeconds() ;
+    var mes=d.getMonth();
+    var dia=d.getDate();
+    //var dia=1;
+    var meridiam=(d.getHours()>=12)?"pm":"am";
+    var th=(dia===1)?"st":(dia===2)?"nd":(dia===3)?"rd":"th";
+    switch(mes){
+        case 0: mes="January"; break;
+        case 1: mes="February"; break;
+        case 2: mes="March"; break;
+        case 3: mes="April"; break;
+        case 4: mes="May"; break;
+        case 5: mes="June"; break;
+        case 6: mes="July"; break;
+        case 7: mes="Auugust"; break;
+        case 8: mes="September"; break;
+        case 9:mes="October"; break;
+        case 10:mes="November"; break;
+        case 11:mes="December"; break;
+    }
+    var timer=document.getElementById("tiempo12");
+    timer.innerHTML=mes+" "+dia+th+" "+d.getFullYear()+" - "+hora+":"+min+" "+meridiam;
+}
+var tiempo12=setInterval(printTime12,1000);
+
 //contructores 
 //Proto arma
 function arma(tipo,dano,peso,velocidad){
@@ -105,5 +183,42 @@ function duel(user,orc){
     }
     else{
         alert("You killed that nigga "+orc.nombre+"!! congrats, now Teabag him");
+    }
+}
+
+//----------
+function showPlayerHP(){
+    document.write(user.nombre +" has "+ user.hp +" hitpoints left <br/>");
+     if (user.hp<=0){
+     clearInterval(playa2);
+     clearInterval(playa);
+ }
+
+}
+function deductPlayerHP(){
+    user.hp-=25;
+}
+function deductOrcHP(){
+    orc.hp-=1;
+}
+
+
+function playerHP(){
+    var playerhpbar = document.getElementById("playerinfo");
+    playerhpbar.innerHTML="HP <progress max=100 value="+user.hp+"></progress> "+user.hp;
+    if (user.hp<=0){
+        playerhpbar.innerHTML="HP <progress value=0></progress> DEAD";
+        //clearInterval(deductphp);
+        clearInterval(playerHP_anim);
+    }
+}
+
+function enemyHP(){
+    var enemyhpbar=document.getElementById("enemyinfo");
+    enemyhpbar.innerHTML = "HP <progress max="+inienemyhp+" value="+orc.hp+"></progress> "+orc.hp;
+    if (orc.hp<=0){
+        enemyhpbar.innerHTML="HP <progress value=0></progress> DEAD";
+        clearInterval(enemyHP_anim);
+        clarInterval(deductOrcHP);
     }
 }
